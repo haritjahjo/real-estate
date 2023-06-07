a3f2bbe60649f017c10f5905d4c41166395e902c
Hari Tjahjo <haritjahjo@gmail.com>
2023-06-07 22:23:20 +0700

    Demo seed users

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Admin
+        DB::table('users')->insert([
+            // Admin
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+                'status' => 'active',
+            ],
+            [
+                // Agent
+
+                'name' => 'Agent',
+                'username' => 'agent',
+                'email' => 'agent@example.com',
+                'password' => Hash::make('password'),
+                'role' => 'agent',
+                'status' => 'active',
+            ],
+            [
+                // User
+
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@example.com',
+                'password' => Hash::make('password'),
+                'role' => 'user',
+                'status' => 'active',
+            ],
+
+        ]);
+    }
+}
