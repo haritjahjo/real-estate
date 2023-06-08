0c99cdd53b51cef98e961b98e8109476bbd14c87
Hari Tjahjo <haritjahjo@gmail.com>
2023-06-08 17:34:29 +0700

    admin login and logout

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
         return view('admin.index');
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    }
+
+    public function AdminLogin(){
+        return view(('admin.admin_login'));
     }
 }
