f9239087d4b17285f2f536ced37150be872467e0
Hari Tjahjo <haritjahjo@gmail.com>
2023-06-09 23:20:18 +0700

    Admin change password

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -64,5 +65,38 @@ class AdminController extends Controller
             'alert-type' => 'success'
         );
         return redirect()->back()->with($notification);    
+    }
+
+    public function AdminChangePassword()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_change_password', compact('profileData'));
+    }
+
+    public function AdminUpdatePassword(Request $request)
+    {
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed'
+        ]);
+
+        if(!Hash::check($request->old_password, Auth::user()->password))
+        {
+            $notification = array(
+                'message' => 'Old Password does not match',
+                'alert-type' => 'error'
+            );
+            return back()->with($notification);
+        };
+
+        User::whereId(auth()->user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+        $notification = array(
+                'message' => 'Password change successfuly',
+                'alert-type' => 'success'
+            );
+            return back()->with($notification);
     }
 }
