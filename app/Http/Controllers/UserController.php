fc27335477b12ad07a75e85b2eafb3924b70dfdb
Hari Tjahjo <haritjahjo@gmail.com>
2023-06-11 16:02:58 +0700

    Add user profile store

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -10,5 +12,41 @@ class UserController extends Controller
     public function Index()
     {
         return view('frontend.index');
+    }
+
+    public function UserProfile()
+    {
+        $id = Auth::user()->id;
+        $userData = User::find($id);
+        return view('frontend.dashboard.edit_profile', compact('userData'));
+    }
+
+    public function UserProfileStore(Request $request)
+    {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+        $data->role = $request->role;
+        
+        
+        if($request->file('photo')){
+            $file = $request->file('photo');
+            @unlink(public_path('upload/user_images/' . $data->photo));
+            $filename = date('YmdHi'.$file->getClientOriginalName());
+            $file->move(public_path('upload/user_images'), $filename);
+            $data['photo'] = $filename;
+        }
+
+        $data->save();
+        $notification = array(
+            'message' => 'User Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);   
     }
 }
